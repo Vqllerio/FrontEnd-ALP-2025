36cda48fa38a190f90d1379e7878884d1f8ad441
Vallerio Rayford Phoatmojo <valleriorayford36@gmail.com>
2025-06-17 18:34:28 +0800

    making data sets for users

---
+++ dataUser.js
@@ -0,0 +1,122 @@
+const users = [
+    {
+        userid: 1,
+        username: "zephyr01",
+        email: "zephyr01@mail.com",
+        password: "pass1234zeph"
+    },
+    {
+        userid: 2,
+        username: "lunar_cat",
+        email: "lunarcat@mail.com",
+        password: "moonMeow987"
+    },
+    {
+        userid: 3,
+        username: "cyberfox",
+        email: "cyberfox@mail.com",
+        password: "cyb3rF0x!"
+    },
+    {
+        userid: 4,
+        username: "shadow_wolf",
+        email: "shadowwolf@mail.com",
+        password: "sh@dow!wolf2025"
+    },
+    {
+        userid: 5,
+        username: "sunray",
+        email: "sunray@mail.com",
+        password: "sunnySideUP!"
+    },
+    {
+        userid: 6,
+        username: "echo_kid",
+        email: "echokid@mail.com",
+        password: "Ech0K1d_444"
+    },
+    {
+        userid: 7,
+        username: "pixel_dust",
+        email: "pixeldust@mail.com",
+        password: "Pixel$parkle12"
+    },
+    {
+        userid: 8,
+        username: "nova_lite",
+        email: "novalite@mail.com",
+        password: "N0vaL1t3!"
+    },
+    {
+        userid: 9,
+        username: "blitzhawk",
+        email: "blitzhawk@mailcom",
+        password: "hawkBltz99"
+    },
+    {
+        userid: 10,
+        username: "dreamytune",
+        email: "dreamytune@mail.com",
+        password: "dreamTune#2025"
+    },
+    {
+        userid: 11,
+        username: "rainyday",
+        email: "rainyday@mail.com",
+        password: "Ra1ny_Dayz"
+    },
+    {
+        userid: 12,
+        username: "ghostbyte",
+        email: "ghostbyte@mail.com",
+        password: "Gh0sty!B1t3"
+    },
+    {
+        userid: 13,
+        username: "firestorm",
+        email: "firestorm@mail.com",
+        password: "f1rest0rm#cool"
+    },
+    {
+        userid: 14,
+        username: "greenbean",
+        email: "greenbean@mail.com",
+        password: "GreenB3an#"
+    },
+    {
+        userid: 15,
+        username: "driftking",
+        email: "driftking@mail.com",
+        password: "driftFast99"
+    },
+    {
+        userid: 16,
+        username: "code_crush",
+        email: "codecrush@mail.com",
+        password: "c0de#2025"
+    },
+    {
+        userid: 17,
+        username: "silvereye",
+        email: "silvereye@mail.com",
+        password: "Silv3r!2025"
+    },
+    {
+        userid: 18,
+        username: "roboz",
+        email: "roboz@mail.com",
+        password: "roboZ_321"
+    },
+    {
+        userid: 19,
+        username: "jellyfish",
+        email: "jellyfish@mail.com",
+        password: "swim@blue"
+    },
+    {
+        userid: 20,
+        username: "hyperbolt",
+        email: "hyperbolt@mail.com",
+        password: "Hyp3rB0lt!!"
+    }
+];
